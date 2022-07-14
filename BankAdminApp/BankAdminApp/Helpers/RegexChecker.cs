2b71ed1cf425f4becd5d1b449276ccfcb489d8b1
ASHLY\Ashly Raju <ashlyr@hcss.io>
2022-07-14 02:51:25 +0200

    Added unit tests.

---
+++ BankAdminApp/BankAdminApp/Helpers/RegexChecker.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace BankingAdminApp.Helpers
+{
+    public class RegexChecker
+    {
+        public bool IsRealNumberMaxLengthFifty(string number)
+        {
+            Regex regex = new Regex(@"[0-9]{1,50}$");
+            if (regex.IsMatch(number))
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public bool IsPositiveNegativeDecimalNumber(string value)
+        {
+            Regex regex = new Regex(@"^-?[0-9]{1,18}(\.[0-9]{1,2})?$");
+            if (regex.IsMatch(value))
+            {
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ BankAdminApp/BankingAdminApp.NUnitTests/RegexNumberTests.cs
@@ -0,0 +1,44 @@
+using BankingAdminApp.Helpers;
+
+namespace BankingAdminApp.UnitTests
+{
+    [TestFixture]
+    public class RegexNumberTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test()]
+        public void CheckIfRealNumberMaxLengthFiftyTest_CheckValid()
+        {
+            RegexChecker regexChecker = new RegexChecker();
+            var expected = regexChecker.IsRealNumberMaxLengthFifty("12345678945612345687945612021458641235478");
+            Assert.AreEqual(expected, true);
+        }
+
+        [Ignore("Ignore")]
+        public void CheckIfRealNumberMaxLengthFiftyTest_CheckNotValid()
+        {
+            RegexChecker regexChecker = new RegexChecker();
+            var expected = regexChecker.IsRealNumberMaxLengthFifty("12.00");
+            Assert.AreEqual(expected, false);
+        }
+
+        public void CheckPositiveNegativeDecimalNumberTest_CheckValid()
+        {
+            RegexChecker regexChecker = new RegexChecker();
+            var expected = regexChecker.IsPositiveNegativeDecimalNumber("-12.2");
+            Assert.AreEqual(expected, true);
+        }
+
+        [Ignore("Ignore")]
+        public void CheckPositiveNegativeDecimalNumberTest_CheckNotValid()
+        {
+            RegexChecker regexChecker = new RegexChecker();
+            var expected = regexChecker.IsPositiveNegativeDecimalNumber("12");
+            Assert.AreEqual(expected, false);
+        }
+    }
+}
